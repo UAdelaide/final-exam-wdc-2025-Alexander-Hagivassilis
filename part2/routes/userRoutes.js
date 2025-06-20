d34482d2ca16a679744c0b4454a295d5927e537b
Alexander Hagivassilis <unknown@student.adelaide.edu.au>
2025-06-20 14:27:57 +0000

    [automatic commit] 2025-06-20 14:27:57 UTC 123.243.200.119

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-    
+    console.log("Logging in");
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     console.log(error);
