fa07a1b6f5f1977c76633c798ef225344671679e
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 04:55:12 +0000

    [automatic commit] 2025-06-20 04:55:11 UTC 123.243.200.119

--- part1/app.js
+++ part1/app.js
@@ -222,16 +222,6 @@ app.get('/api/walkers/summary', async (req, res) => {
     }
 });
 
-import { createApp, ref } from 'vue';
-
-createApp({
-  setup() {
-    return {
-      count: ref(0)
-    }
-  }
-}).mount('#app');
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
+import { createApp, ref } from 'vue';
 
+createApp({
+  setup() {
+    return {
+      count: ref(0)
+    }
+  }
+}).mount('#app');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
