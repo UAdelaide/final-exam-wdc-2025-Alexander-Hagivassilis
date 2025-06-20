3cc2f0b53a88f810ea9deb9a5bafd0ffdef13808
Alexander Hagivassilis <unknown@student.adelaide.edu.au>
2025-06-20 14:28:18 +0000

    [automatic commit] 2025-06-20 14:28:17 UTC 123.243.200.119

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -38,7 +38,6 @@ router.get('/me', (req, res) => {
 // POST login
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
-  console.log(req.body);
 
   try {
     const [rows] = await db.query(`
