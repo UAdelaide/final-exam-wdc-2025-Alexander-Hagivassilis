6cab27b06d0df9969693c3e00cd1c0fcb6d69f49
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 02:52:43 +0000

    [automatic commit] 2025-06-20 02:52:43 UTC 123.243.200.119

--- part1/app.js
+++ part1/app.js
@@ -84,7 +84,7 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-        const open_requests = await db.execute('SELECT Users.username, Dogs.name, WalkRequests.requested_time, WalkRequests.requested_')
+        const open_requests = await db.execute('SELECT Users.username, Dogs.name, WalkRequests.requested_time, WalkRequests.duration_minutes, ')
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch open walk requests' });
     }
