048388aa6088e86b4bf572c1fb1bd673a71b25aa
Alexander Hagivassilis <unknown@student.adelaide.edu.au>
2025-06-21 02:13:04 +0000

    [automatic commit] 2025-06-21 02:13:03 UTC 123.243.200.119

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -32,7 +32,7 @@ router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
-  res.json( {user: req.session.user, );
+  res.json({ user: req.session.user, user: req.session.user_id, role: req.);
 });
 
 // POST login
